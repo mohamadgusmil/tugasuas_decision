0bab146f6e05dea91e9f04175a4e2cdba8f17a89
tugasuas_decison <gusmilsaparudin3@gmail.com>
2021-06-04 16:49:53 +0700

    tugas_decision

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Configuration Database
+ */
+return array(
+    'host' => "localhost",
+    'username' => "root",
+    'password' => "",
+    'dbname' => "tugas_decision",
+    // 'port' => "", // sementara menggunakan port bawaan
+);
+
+?>
